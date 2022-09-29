6da7b41608816ad09f695b0f01899df1cc877259
Shriram Rathod <shriramrathod47@gmail.com>
2022-09-29 06:59:59 +0530

    ReactJs project

---
+++ src/CoinItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CoinItem = (props) =>{
+    <div className='coin-row'>
+            <p>{props.coins.market_cap_rank}</p>
+
+    </div>
+    }
+export default CoinItem    
