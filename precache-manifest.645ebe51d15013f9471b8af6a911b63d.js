2e76be09bcefcbdac3a1b261c3242f57a774cb12
Shriram Rathod <shriramrathod47@gmail.com>
2022-09-29 08:31:14 +0530

    Updates

---
+++ precache-manifest.645ebe51d15013f9471b8af6a911b63d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43539032a65979e0c3eb5b5958148ef",
+    "url": "/Crypto-tracker-App/index.html"
+  },
+  {
+    "revision": "a2b8da15e04acf7234de",
+    "url": "/Crypto-tracker-App/static/css/main.7f2ad3be.chunk.css"
+  },
+  {
+    "revision": "ada2c03c0125937f0ed6",
+    "url": "/Crypto-tracker-App/static/js/2.aeeb7782.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Crypto-tracker-App/static/js/2.aeeb7782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b8da15e04acf7234de",
+    "url": "/Crypto-tracker-App/static/js/main.a692e4e3.chunk.js"
+  },
+  {
+    "revision": "e5f524302dd0335d3b3b",
+    "url": "/Crypto-tracker-App/static/js/runtime-main.160828af.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae8abf2482fe75f15a6d4aaf79076305.js"
+  "/Crypto-tracker-App/precache-manifest.645ebe51d15013f9471b8af6a911b63d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Crypto-tracker-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
